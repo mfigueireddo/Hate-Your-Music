e540d4ba6ca1d785bf2d9565daececd156559803
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2024-05-23 20:19:38 +0000

    User register page created

--- hateyourmusic/models.py
+++ hateyourmusic/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
-from django.utils import timezone
 
 fs_picture = FileSystemStorage("/media/profile_pictures")
 fs_background = FileSystemStorage("/media/profile_background")
 
 class Profile(models.Model):
-  user = models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+  user = models.OneToOneField(User,null=False, on_delete=models.CASCADE)
   biography = models.TextField(blank=True)
   name = models.TextField(blank=True)
   location = models.TextField(blank=True)
   url = models.URLField(blank=True)
-  birthday = models.DateField(default = timezone.now, blank=True)
-  date_joined = models.DateField(default = timezone.now, blank=True)
+  birthday = models.DateField(null=True, blank=True)
+  date_joined = models.DateField(auto_now_add=True)
   profile_picture = models.ImageField(storage = fs_picture, blank=True)
   profile_background = models.ImageField(storage = fs_background, blank=True)
--- hateyourmusic/views.py
+++ hateyourmusic/views.py
@@ -1,4 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile
+from django.contrib.auth.models import User
+from django.core.validators import validate_email, URLValidator
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+
 
 def home(request):
   return render(request, "home.html")
+
+def create_user(request):
+  if request.method == "POST":
+
+    username = request.POST["username"]
+
+    if User.objects.filter(username=username).first():
+      return render(request, "register.html", context={"username_error":True})
+
+    email = request.POST["email"]
+
+    try:
+      validate_email(email)
+    except ValidationError:
+      return render(request, "register.html", context={"invalid_email": True})
+    
+    if User.objects.filter(email=email).first():
+      return render(request, "register.html", context={"email_error":True})
+
+    password = request.POST["password"]
+    
+    try:
+      validate_password(password)
+    except Exception as error_message:
+      return render(request, "register.html", context={"password_error": True, "error_message": error_message})
+      
+    user = User.objects.create_user(
+      username,
+      email,
+      password
+    )
+
+    user.save()
+
+    url = request.POST["url"]
+    
+    if url:
+      url_validator =  URLValidator()
+      try:
+        url_validator(url)
+      except ValidationError:
+        return render(request, "register.html", context={"invalid_url": True})
+
+    name = request.POST["name"]
+
+    if not name:
+      name = username
+    
+    Profile.objects.create(
+      user=user,
+      biography = request.POST["biography"],
+      name = name,
+      location = request.POST["location"],
+      url = url,
+      birthday = request.POST["birthday"] if request.POST["birthday"] else None,
+      profile_picture = request.POST["profile_picture"],
+      profile_background = request.POST["profile_background"]
+    )
+
+    return redirect("home")
+  
+  return render(request,"register.html")
