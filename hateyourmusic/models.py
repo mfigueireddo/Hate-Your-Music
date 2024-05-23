9959b3e01df14c5728e49ac0ca8ffca580d53009
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2024-05-23 14:29:19 +0000

    Profile model - DONE

--- hateyourmusic/models.py
+++ hateyourmusic/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.utils import timezone
 
-# Create your models here.
+fs_picture = FileSystemStorage("/media/profile_pictures")
+fs_background = FileSystemStorage("/media/profile_background")
+
+class Profile(models.Model):
+  user = models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+  biography = models.TextField(blank=True)
+  name = models.TextField(blank=True)
+  location = models.TextField(blank=True)
+  url = models.URLField(blank=True)
+  birthday = models.DateField(default = timezone.now, blank=True)
+  date_joined = models.DateField(default = timezone.now, blank=True)
+  profile_picture = models.ImageField(storage = fs_picture, blank=True)
+  profile_background = models.ImageField(storage = fs_background, blank=True)
